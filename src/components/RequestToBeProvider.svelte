1686210f52ae4d0e0907c5a2b6aa876bf7eac154
Robert La Ferla <robert@onemsg.co>
2024-01-30 12:58:05 -0500

    refactor: SelectNetworkAndAccount component and added styling

--- src/components/RequestToBeProvider.svelte
+++ src/components/RequestToBeProvider.svelte
@@ -2,17 +2,24 @@
   import { transactionSigningAddress, dotApi } from '$lib/stores';
   import { submitRequestToBeProvider } from '$lib/connections';
   import { onMount } from 'svelte';
+  import type { web3Accounts, web3Enable, web3FromSource } from '@polkadot/extension-dapp';
   import { defaultDotApi } from '$lib/storeTypes';
   import type { DotApi } from '$lib/storeTypes';
   import { isFunction } from '@polkadot/util';
   import { isLocalhost, createMailto } from '$lib/utils';
-  import { ApiPromise } from '@polkadot/api';
+  import type { ApiPromise } from '@polkadot/api';
   import TransactionStatus from '$components/TransactionStatus.svelte';
+  import BlockSection from './BlockSection.svelte';
+  import { user } from '$lib/stores/userStore';
+  import SelectNetworkAndAccount from './SelectNetworkAndAccount.svelte';
+  import { nonProviderAccountsStore } from '$lib/stores/accountsStore';
 
   let newProviderName = '';
   let localDotApi: DotApi = defaultDotApi;
-  let web3FromSource;
-  let web3Enable;
+  let thisWeb3FromSource: typeof web3FromSource;
+  let thisWeb3Enable: typeof web3Enable;
+  let thisWeb3Accounts: typeof web3Accounts;
+
   let showTransactionStatus = false;
   let mailTo = createMailto('hello@frequency.xyz', 'Request to be a Provider', '');
   export let txnStatuses: Array<string> = [];
@@ -27,8 +34,9 @@
 
   onMount(async () => {
     const extension = await import('@polkadot/extension-dapp');
-    web3FromSource = extension.web3FromSource;
-    web3Enable = extension.web3Enable;
+    thisWeb3FromSource = extension.web3FromSource;
+    thisWeb3Enable = extension.web3Enable;
+    thisWeb3Accounts = extension.web3Accounts;
   });
 
   transactionSigningAddress.subscribe((addr) => (signingAddress = addr));
@@ -58,10 +66,10 @@
         txnFinished
       );
     } else {
-      if (isFunction(web3FromSource) && isFunction(web3Enable)) {
-        const extensions = await web3Enable('Frequency parachain provider dashboard: Proposing to be provider');
+      if (isFunction(thisWeb3FromSource) && isFunction(thisWeb3Enable)) {
+        const extensions = await thisWeb3Enable('Frequency parachain provider dashboard: Proposing to be provider');
         if (extensions.length !== 0) {
-          const injectedExtension = await web3FromSource(signingKeys.meta.source);
+          const injectedExtension = await thisWeb3FromSource(signingKeys.meta.source);
           await submitRequestToBeProvider(
             localDotApi.api as ApiPromise,
             injectedExtension,
@@ -76,8 +84,8 @@
           return;
         }
       } else {
-        console.error('web3FromSource is function? ', isFunction(web3FromSource));
-        console.error('web3Enable is function? ', isFunction(web3Enable));
+        console.error('web3FromSource is function? ', isFunction(thisWeb3FromSource));
+        console.error('web3Enable is function? ', isFunction(thisWeb3Enable));
       }
     }
   };
@@ -87,34 +95,29 @@
     return;
   };
   const clearTxnStatuses = () => (txnStatuses = new Array<string>());
+
 </script>
 
 <div id="request-to-be-provider" class="action-card basis-1/2">
-  <h2>Request to Be a Provider</h2>
-  <h3>What is a Provider?</h3>
-  <p>A Provider is an MSA holder on Frequency with special permissions.</p>
-  <ol>
-    <li>They can pay for transactions with Capacity as well as Frequency token.</li>
-    <li>They can be permitted to post certain transactions on another MSA's behalf, also known as delegation.</li>
-    <li>An MSA can stake token to generate Capacity, and designate a Provider to receive that Capacity.</li>
-  </ol>
-  <p>Anyone with an MSA ID on Frequency's Mainnet who wants to become a Provider must follow this process:</p>
-  <ol>
-    <li>Submit an on-chain transaction to request be become a provider by filling in and submitting the form below.</li>
-    <li>
-      <a href={mailTo} class="underline"> Contact the Frequency Council </a>
-      and inform them that you have requested to become a Provider, and provide them with your MSA Id.
-    </li>
-  </ol>
-  <form>
-    <label for="providerNameRtB">Provider name</label>
-    <input id="providerNameRtB" placeholder="Short name" maxlength="16" bind:value={newProviderName} />
-    <div class="w-350 flex justify-between">
-      <button on:click|preventDefault={doProposeToBeProvider} id="request-2b-provider-btn" class="btn-primary">
-        Submit Request To Be Provider</button
-      >
-      <button on:click|preventDefault={cancelAction} class="btn-cancel">Cancel</button>
+  <BlockSection title="Become a provider">
+    <form class="flex flex-col gap-4 w-[320px]">
+      <SelectNetworkAndAccount accountsStore={$nonProviderAccountsStore}></SelectNetworkAndAccount>
+      <label for="providerNameRtB" class="label">Provider name</label>
+      <input id="providerNameRtB" placeholder="Short name" maxlength="16" bind:value={newProviderName} />
+      <div class="w-350 flex justify-between">
+        <button on:click|preventDefault={doProposeToBeProvider} id="request-2b-provider-btn" class="btn-primary">Create Provider</button>
+        <button on:click|preventDefault={cancelAction} class="btn-no-fill">Cancel</button>
+      </div>
+    </form>
+  </BlockSection>
+  <BlockSection title="More Info">
+    <div
+      style="width: 100%; color: white; font-size: 12px; font-family: Poppins; font-weight: 400; line-height: 18px; word-wrap: break-word"
+    >
+      For developer and testing convenience, on Testnet, anyone with an MSA who wishes to become a Provider may simply
+      submit a createProvider transaction.<br /><br />This action will register the MSA Id that is controlled by the
+      selected Transaction Signing Address above. Any control key for the MSA Id can submit the transaction.
     </div>
-  </form>
+  </BlockSection>
 </div>
 <TransactionStatus bind:showSelf={showTransactionStatus} statuses={txnStatuses} />
